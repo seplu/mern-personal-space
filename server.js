a35de78d69997af7089db6213a7182a1dc9b53f6
Sebastian Pludowski <sepludowski@gmail.com>
2023-10-04 22:42:17 +0200

    Enable code formatting with Prettier

--- server.js
+++ server.js
@@ -1,26 +1,26 @@
-import dotenv from 'dotenv';
-import express from 'express';
-import morgan from 'morgan';
+import dotenv from "dotenv";
+import express from "express";
+import morgan from "morgan";
 
 const app = express();
 dotenv.config();
 
-if(process.env.NODE_ENV !== 'production'){
-    app.use(morgan('dev'));
+if (process.env.NODE_ENV !== "production") {
+  app.use(morgan("dev"));
 }
 
-app.get('/', (req, res) => {
-    res.send('Personal Space Welcome');
-})
+app.get("/", (req, res) => {
+  res.send("Personal Space Welcome");
+});
 
 const port = process.env.PORT || 5100;
 
 const start = async () => {
-    try {
-        app.listen(port, () => console.log(`Server is listening on port ${port}`));
-    } catch (error) {
-        console.log(error);
-    }
-}
+  try {
+    app.listen(port, () => console.log(`Server is listening on port ${port}`));
+  } catch (error) {
+    console.log(error);
+  }
+};
 
-start().then(() => console.log('Server is started'));
+start().then(() => console.log("Server is started"));
