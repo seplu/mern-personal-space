b37f714b73597ee3509d9acadcf33c049cf51446
Sebastian Pludowski <sepludowski@gmail.com>
2023-12-21 07:32:39 +0100

    Use appContext

--- client/src/index.js
+++ client/src/index.js
@@ -1,9 +1,15 @@
+import App from "./App";
 import React from "react";
 import ReactDOM from "react-dom/client";
+
+import "./index.css";
+import { AppProvider } from "./context/appContext";
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <React.StrictMode>
-    <div>Hello World</div>
+    <AppProvider>
+      <App />
+    </AppProvider>
   </React.StrictMode>,
 );
