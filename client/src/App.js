0347e955859fdf194d244d78a6b068785c7cd015
Sebastian Pludowski <sepludowski@gmail.com>
2024-01-01 12:04:17 +0100

    Import react-router-dom

--- client/src/App.js
+++ client/src/App.js
@@ -1,15 +1,15 @@
 import Navbar from "./components/Navbar";
 import NavigationBar from "./components/NavigationBar";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import "bootstrap/dist/css/bootstrap.css";
 
 function App() {
   return (
-    <div className="App">
-      <Navbar />
-    </div>
+    <Router>
       <div className="App">
         <NavigationBar />
       </div>
+    </Router>
   );
 }
 
