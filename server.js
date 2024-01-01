9f33c93fd374605e2a787d8922a7c394e6070051
Sebastian Pludowski <sepludowski@gmail.com>
2024-01-01 13:42:57 +0100

    [BE] Configure Mongo

--- server.js
+++ server.js
@@ -5,6 +5,8 @@ import morgan from "morgan";
 const app = express();
 dotenv.config();
 
+import connectDB from "./db/connect.js";
+
 if (process.env.NODE_ENV !== "production") {
   app.use(morgan("dev"));
 }
@@ -17,6 +19,7 @@ const port = process.env.PORT || 5100;
 
 const start = async () => {
   try {
+    await connectDB(process.env.MONGO_URL);
     app.listen(port, () => console.log(`Server is listening on port ${port}`));
   } catch (error) {
     console.log(error);
