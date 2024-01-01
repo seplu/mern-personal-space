c412e9fffc93e7a74ce3ae1fed2de958e6055828
Sebastian Pludowski <sepludowski@gmail.com>
2024-01-01 12:07:05 +0100

    Added Car Dummy page

--- client/src/App.js
+++ client/src/App.js
@@ -1,4 +1,5 @@
-import Navbar from "./components/Navbar";
+import Car from "./pages/Car";
+import Finance from "./pages/Finance";
 import Home from "./pages/Home";
 import NavigationBar from "./components/NavigationBar";
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
@@ -11,6 +12,8 @@ function App() {
         <NavigationBar />
         <Routes>
           <Route path="/" element={<Home />} />
+          <Route path="/car" element={<Car />} />
+          <Route path="/finance" element={<Finance />} />
         </Routes>
       </div>
     </Router>
