927a7bc5c4c320192a0781106b1f8896b88b93a3
Sebastian Pludowski <sepludowski@gmail.com>
2024-01-03 00:04:36 +0100

    Handle errors

--- server.js
+++ server.js
@@ -1,9 +1,13 @@
 import dotenv from "dotenv";
 import express from "express";
+import "express-async-errors";
 import morgan from "morgan";
 
 const app = express();
 dotenv.config();
+
+// Middleware
+import errorHandlerMiddleware from "./middleware/error-handler.js";
 
 if (process.env.NODE_ENV !== "production") {
   app.use(morgan("dev"));
@@ -12,6 +16,8 @@ if (process.env.NODE_ENV !== "production") {
 app.get("/", (req, res) => {
   res.send("Personal Space Welcome");
 });
+// Middleware Usage
+app.use(errorHandlerMiddleware);
 
 const port = process.env.PORT || 5100;
 
